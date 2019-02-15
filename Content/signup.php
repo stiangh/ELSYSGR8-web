ce828ae86fc033da6e14a3d8c37b36e4eb29a9ad
stiangh <32125631+stiangh@users.noreply.github.com>
2019-02-15 21:16:42 +0100

    Added password requirements client-side.

--- Content/signup.php
+++ Content/signup.php
@@ -1,10 +1,24 @@
+<script>
+    window.onload = oppstart_signup;
+
+    function oppstart_signup() {
+        let form = document.getElementById("signup");
+        let inputs = form.getElementsByTagName("INPUT");
+        for (let i = 0; i < inputs.length; i++) {
+            let x = inputs[i];
+            x.addEventListener('invalid', function() {this.style.borderColor = "red"; if (this.dataset.hasOwnProperty('ermsg')) {this.setCustomValidity(this.dataset.ermsg)}});
+            x.addEventListener('input', function() {this.style.borderColor = "initial"; this.setCustomValidity('');});
+        }
+        form.pwd.addEventListener('input', function() {form.pwd2.pattern = this.value;});
+    }
+</script>
 <h1>Registrer Bruker</h1>
 <form method="POST" action="Includes/signup-h.php" name="signup" id="signup">
     <input name="email" type="email" placeholder="E-mail"><br>
     <input name="fname" type="text" placeholder="First Name"><br>
     <input name="sname" type="text" placeholder="Last Name"><br>
-    <input name="pwd" type="password" placeholder="Password"><br>
-    <input name="pwd2" type="password" placeholder="Re-enter Password"><br>
+    <input name="pwd" type="password" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" data-ermsg='Passordet må inneholde minst 8 karakterer, med minst én stor bokstav, liten bokstav og siffer.' placeholder="Password"><br>
+    <input name="pwd2" type="password" data-ermsg='Dette feltet må være likt det forrige.' placeholder="Re-enter Password"><br>
     <button name="submit" type="submit" form="signup" value="mitsub">Submit</button><br>
 </form>
 <?php
